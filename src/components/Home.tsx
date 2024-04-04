0362bb0361cc8a0d1c39ff5956ea274d4651e041
Chetn11 <cnpatil00@gmail.com>
2024-04-04 09:13:02 +0530

    basic structure done

---
+++ src/components/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import styles from "./Home.module.css"
+
+function Home() {
+  return (
+    <div className={styles.main}>
+        <div className={styles.container}>
+            <div className={styles.customer_list}>
+
+            </div>
+
+            <div className={styles.customer_details}>
+
+            </div>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Home
